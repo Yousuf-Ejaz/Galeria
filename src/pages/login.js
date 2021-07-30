ce56e96be9c483c8f20c883f50cda281be199de6
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2021-07-30 21:53:33 +0530

    update: additions

---
+++ src/pages/login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+export default function Login()  {
+    const history = useHistory();
+    return (
+        <p>I am the login Page</p>
+    );
+}
