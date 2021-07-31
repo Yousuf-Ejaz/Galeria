19b12920880af587c5663d51f88409e32d9ca9eb
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2021-08-01 04:16:41 +0530

    update: UI

--- src/components/post/footer.js
+++ src/components/post/footer.js
@@ -3,8 +3,18 @@ import PropTypes from "prop-types";
 export default function Footer({ caption, username }) {
   return (
     <div className="p-4 pt-2 pb-1">
-      <span className="mr-1 font-bold">{username}</span>
-      <span className="italic">{caption}</span>
+      <span className="mr-1 font-bold mb-3">{username}</span>
+      <span className="italic mb-3">{caption}</span>
+      <div className='border-t border-gray-primary border-b pb-3 specs'>
+        <h4 className="mr-1 font-bold mt-1">PRODUCT DETAILS</h4>
+        Set content: 1 Wall art <br />
+        Type: Oil Painting <br />
+        Colour: White, orange and blue <br />
+        Pattern: Abstract Morden Art <br />
+        Material: Canvas <br />
+        <h4 className="mr-1 font-bold mt-2">Size & Fit </h4>
+        Dimension: 60.9 cm x 91.4 cm (Height x Width) <br />
+      </div>
     </div>
   );
 }
