6d88d8b7e92ed90a08a38d72403a1c42c1eface4
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2021-08-01 02:04:03 +0530

    add: final stages

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,15 +1,20 @@
-import useUser from "../../hooks/use-user";
-import User from "./user"
+import { useContext } from "react";
+import User from "./user";
 import Suggestions from "./suggestions";
-
-
+import LoggedInUserContext from "../../context/logged-in-user";
 
 export default function Sidebar() {
-    const {user:{fullName, username, userId}} = useUser();
-    return (
-        <div className="p-4">
-            <User username={username} fullName={fullName} />
-            <Suggestions userId={userId} />
-        </div>
-    );
+  const { user: { docId = "", fullName, username, userId, following } = {} } =
+    useContext(LoggedInUserContext);
+
+  return (
+    <div className="p-4">
+      <User username={username} fullName={fullName} />
+      <Suggestions
+        userId={userId}
+        following={following}
+        loggedInUserDocId={docId}
+      />
+    </div>
+  );
 }
